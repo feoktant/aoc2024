3d4593be4ec6cd85a0069815ab4cbb10105e539b
Anton Feoktistov <anton.feoktistov@gmail.com>
2024-12-02 21:19:10 +0200

    Day 2: solution inspired by ahaan1984
    Like this one, still think it should be possible to get rid of two sorts

--- src/main/scala/day2.scala
+++ src/main/scala/day2.scala
@@ -36,3 +36,22 @@ def permutations(l: List[Int]): List[List[Int]] =
   val answer2 = args.map(l => (l :: permutations(l)).map(toLevelList))
     .count(_.exists(isSafe))
   println(answer2)
+
+  println("Solution by ahaan1984, rewritten from Python to Scala")
+  val altAnswer2 = args.count(checkSafeDampener)
+  println(altAnswer2)
+
+// ------ Inspired by https://x.com/ahaan1984/status/1863607699126296614
+
+def checkSafe(level: List[Int]): Boolean =
+  @scala.annotation.tailrec
+  def loop(lvl: List[Int]): Boolean = lvl match
+    case h1 :: h2 :: _ =>
+      if (1 to 3).contains((h1 - h2).abs) then loop(lvl.tail) else false
+    case _ => true
+
+  Set(level.sorted, level.sorted.reverse).contains(level) && loop(level)
+
+def checkSafeDampener(level: List[Int]): Boolean =
+  level.indices.exists: idx =>
+    checkSafe(level.take(idx) ++ level.drop(idx + 1))
