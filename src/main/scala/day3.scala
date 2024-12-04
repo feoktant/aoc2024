4335cc200b2d8aa04a5c1e593b65af75323cbf57
Anton Feoktistov <anton.feoktistov@gmail.com>
2024-12-04 11:50:03 +0200

    Changed cmd parser to parser func as conflicting with other days

--- src/main/scala/day3.scala
+++ src/main/scala/day3.scala
@@ -1,11 +1,10 @@
 package io.feoktant
 
 import scala.io.Source
-import scala.util.{CommandLineParser, Using}
+import scala.util.Using
 
-given CommandLineParser.FromString[List[String]] with
-  def fromString(value: String): List[String] =
-    Using.resource(Source.fromResource(value))(_.getLines().toList)
+def parse(value: String): List[String] =
+  Using.resource(Source.fromResource(value))(_.getLines().toList)
 
 def toPairs(lines: List[String]): List[(Int, Int)] =
   val regex = raw"mul\((\d{1,3}),(\d{1,3})\)".r
@@ -16,7 +15,8 @@ def toPairs(lines: List[String]): List[(Int, Int)] =
 def calculate(lines: List[String]): Int =
   toPairs(lines).map { case (t1, t2) => t1 * t2 }.sum
 
-@main def day3(lines: List[String]): Unit =
+@main def day3(args: String*): Unit =
+  val lines = parse(args(0))
   val filteredLines = lines.mkString
     .split(raw"do\(\)")
     .flatMap(_.split(raw"don't\(\)").headOption)
@@ -24,3 +24,17 @@ def calculate(lines: List[String]): Int =
 
   println(calculate(lines))
   println(calculate(filteredLines))
+
+//  ------ Inspired by https://x.com/SammedC94865641/status/1863874091205484832
+  val (sum, _) = raw"mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)".r
+    .findAllMatchIn(lines.mkString).foldLeft(0 -> true):
+      case ((acc, isEnabled), mat) =>
+        mat.group(0) match
+          case "do()" => acc -> true
+          case "don't()" => acc -> false
+          case _ if !isEnabled => acc -> false
+          case _ if isEnabled =>
+            val n1 = mat.group(1).toInt
+            val n2 = mat.group(2).toInt
+            (n1 * n2 + acc) -> isEnabled
+  println(sum)
