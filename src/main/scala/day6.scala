244efe4df3d0a607e2910282ad00081ea4c36a44
Anton Feoktistov <anton.feoktistov@gmail.com>
2024-12-07 16:14:42 +0200

    Day 5: working solution

--- src/main/scala/day6.scala
+++ src/main/scala/day6.scala
@@ -3,25 +3,24 @@ package io.feoktant
 import scala.annotation.tailrec
 import scala.io.Source
 import scala.util.{CommandLineParser, Using}
+import Direction._
 
 enum Direction:
   case Up, Right, Down, Left
-  def next(point: (Int, Int)): (Int, Int) =
-    val (x, y) = point
-    this match
+  def nextDirection: Direction =
+    val index = if (ordinal == Direction.values.length - 1) 0 else ordinal + 1
+    Direction.fromOrdinal(index)
+
+case class GuardPos(point: (Int, Int), dir: Direction):
+  val (x, y) = point
+  def turnRight: GuardPos = copy(dir = dir.nextDirection)
+  def nextPos: GuardPos =
+    val nextPoint = dir match
       case Up => (x - 1) -> y
       case Left => x -> (y - 1)
       case Down => (x + 1) -> y
       case Right => x -> (y + 1)
-
-case class GuardPos(point: (Int, Int), dir: Direction):
-  private def rotate(t: (Int, Int)): (Int, Int) = (-t._2, t._1)
-  def turnRight: GuardPos =
-    val newOrd = Direction.fromOrdinal((dir.ordinal + 1) % Direction.values.length)
-    copy(dir = newOrd)
-  def nextPos: GuardPos = GuardPos(dir.next(x, y), dir)
-  def x: Int = point._1
-  def y: Int = point._2
+    GuardPos(nextPoint, dir)
 
 case class Day6Input(
   map: Array[Array[Char]],
@@ -30,37 +29,36 @@ case class Day6Input(
 
 given CommandLineParser.FromString[Day6Input] with
   def fromString(value: String): Day6Input =
-    val map: Array[Array[Char]] = Using.resource(Source.fromResource(value)) {
-      _.getLines().toList.map(_.toCharArray)}
+    val theMap = Using.resource(Source.fromResource(value)) {
+        _.getLines().toList.map(_.toCharArray) }
       .toArray
-
-    val pos = (for {
-      x <- map.indices
-      y <- map(x).indices
-      if map(x)(y) == '^'
-    } yield GuardPos((x, y), Direction.Up)).toList.head
-
-    Day6Input(map, pos)
+    val startPoint = theMap.zipWithIndex.find((arr, x) => arr.contains('^'))
+      .map((arr, x) => x -> arr.indexOf('^'))
+      .getOrElse(throw new IllegalArgumentException("Invalid situation map"))
+    Day6Input(theMap, GuardPos(startPoint, Up))
 
 @main def day6(input: Day6Input): Unit =
-  val Day6Input(map, guardPos) = input
-  val indices = map.indices.flatMap(x => map(x).indices.map(x -> _)).toSet
+  val Day6Input(theMap, guardPos) = input
+  val indices = theMap.indices.flatMap(x => theMap(x).indices.map(x -> _)).toSet
+  val hashPoints = (theMap.zipWithIndex.collect:
+    case (arr, x) if arr.contains('#') =>  x -> arr.indexOf('#')).toSet
 
-  def getPath(start: GuardPos, obstacle: Option[(Int, Int)] = None): Option[Set[GuardPos]] =
+  def getPath(start: GuardPos, obstacle: Option[(Int, Int)] = None): Option[List[GuardPos]] =
     @tailrec
-    def loop(pos: GuardPos, acc: Set[GuardPos]): Option[Set[GuardPos]] =
+    def loop(pos: GuardPos, acc: Set[GuardPos]): Option[List[GuardPos]] =
       val newPos = pos.nextPos
-      if (!indices.contains(newPos.point)) Some(acc)
-      else if (map(newPos.x)(newPos.y) == '#' || obstacle.contains(newPos.point))
+      if (!indices.contains(newPos.point)) Some(acc.toList)
+      else if (theMap(newPos.x)(newPos.y) == '#' || obstacle.contains(newPos.point))
         loop(pos.turnRight, acc)
       else if (acc(newPos)) None
       else loop(newPos, acc + newPos)
     loop(start, Set(start))
 
   // answer1
-  val visitedPoints = getPath(guardPos).getOrElse(Set.empty).map(_.point)
+  val visitedPoints = getPath(guardPos).getOrElse(List.empty).map(_.point).distinct
   println(visitedPoints.size)
 
-  val pathWithObstacle = (visitedPoints - guardPos.point)
-    .map(p => getPath(guardPos, Some(p)))
-  println(pathWithObstacle.count(_.isEmpty))
+  // answer2
+  val endlessLoopCount = visitedPoints.filterNot(_ == guardPos.point)
+    .count(p => getPath(guardPos, Some(p)).isEmpty)
+  println(endlessLoopCount)
