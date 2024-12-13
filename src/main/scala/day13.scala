95b15759ac8ac4715a26fd87af06fa5498bd82fb
Anton Feoktistov <anton.feoktistov@gmail.com>
2024-12-14 01:54:28 +0200

    Day 13: changing Int to Longs

--- src/main/scala/day13.scala
+++ src/main/scala/day13.scala
@@ -3,15 +3,6 @@ package io.feoktant
 import scala.io.Source
 import scala.util.{CommandLineParser, Using}
 
-//import scala.annotation.tailrec
-//
-//@tailrec
-//def gcd(a: Int, b: Int): Int =
-//  if (b == 0) a else gcd(b, a % b)
-//
-//def lcm(a: Int, b: Int): Int =
-//  (a * b) / gcd(a, b)
-
 given CommandLineParser.FromString[List[ClawMachine]] with
   def fromString(value: String): List[ClawMachine] =
     val str = raw"Button A: X\+(\d{1,3}), Y\+(\d{1,3})Button B: X\+(\d{1,3}), Y\+(\d{1,3})Prize: X=(\d{3,5}), Y=(\d{3,5})".r
@@ -19,36 +10,37 @@ given CommandLineParser.FromString[List[ClawMachine]] with
       _.getLines().toList.grouped(4).toList.map(_.mkString).collect {
         case str(xA, yA, xB, yB, x, y) =>
           ClawMachine(
-            xA.toInt -> yA.toInt,
-            xB.toInt -> yB.toInt,
-            x.toInt,
-            y.toInt,
+            xA.toLong -> yA.toLong,
+            xB.toLong -> yB.toLong,
+            x.toLong,
+            y.toLong,
           )
       })
 
 case class ClawMachine(
-  a: (Int, Int),
-  b: (Int, Int),
-  x: Int,
-  y: Int,
+  a: (Long, Long),
+  b: (Long, Long),
+  x: Long,
+  y: Long,
 ) {
-  def solve: Option[(Int, Int)] =
+  def solve: Option[(Long, Long)] =
     val (xA, yA) = a
     val (xB, yB) = b
     val A = (x * yB - y * xB) / (xA * yB - xB * yA)
     val B = (x * yA - y * xA) / (xB * yA - yB * xA)
     Some(A -> B)
       .filter((a, b) => ((a * xA + b * xB) == x) && ((a * yA + b * yB) == y))
-      .filter((a, b) => a <= 100 && b <= 100)
       .filter((a, b) => a >= 0 && b >= 0)
 }
 
-
-def toTokens(a: Int, b: Int): Int = a*3 + b
+def toTokens(a: Long, b: Long): Long = a*3 + b
 
 @main def day13(input: List[ClawMachine]): Unit =
-//  val answer1 = ClawMachine(94 -> 34, 22 -> 67, 8400, 5400).solve.map(toTokens)
-//  val answer2 = ClawMachine(26 -> 66, 67 -> 21, 12748, 12176).solve.map(toTokens)
-//  val answer3 = ClawMachine(17 -> 86, 84 -> 37, 7870, 6450).solve.map(toTokens)
-  val answer = input.flatMap(_.solve).map(toTokens).sum
+  val answer = input.flatMap(_.solve)
+    .filter((a, b) => a <= 100 && b <= 100)
+    .map(toTokens).sum
   println(answer)
+
+  val answer2 = input.map(c => c.copy(x = c.x + 10000000000000L, y = c.y + 10000000000000L))
+    .flatMap(_.solve).map(toTokens).sum
+  println(answer2)
